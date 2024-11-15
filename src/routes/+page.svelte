c5f256bd181551f06159aee8890bbd5ae6e99305
Pranav <pranav.pooruli@gmail.com>
2024-11-15 19:48:59 +0530

    restarting the project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,6 @@
         "100080006020400070003000500004007010500030008060200900007000200080009030900060004";
 </script>
 
-<h1>sudokracker</h1>
+<h1>SudoKracker</h1>
 <hr />
 <a href="/analyze/{grid}">Analyze</a>
